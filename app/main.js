c8f8f5ae20a33b71f9a348657caee81d5f90d6bb
Faiz Ul-Haque <faizulhaque@tenpearls.com>
2016-11-03 17:15:45 +0500

    first commit

---
+++ app/main.js
@@ -0,0 +1,8 @@
+"use strict";
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var app_module_1 = require('./app.module');
+var platform = platform_browser_dynamic_1.platformBrowserDynamic();
+platform.bootstrapModule(app_module_1.AppModule); /**
+ * Created by shahbaz.ali on 11/3/2016.
+ */
+//# sourceMappingURL=main.js.map
---
+++ app/main.ts
@@ -0,0 +1,6 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);/**
+ * Created by shahbaz.ali on 11/3/2016.
+ */
