3368554fedcb870364bf38d4104bed1447abf664
Faiz Ul-Haque <faizulhaque@tenpearls.com>
2016-11-04 12:23:35 +0500

    Part 4 of tutorial: Refactored the app into sepereate components.

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import {FormsModule} from "@angular/forms";
+import {HeroDetailComponent} from "./hero-detail.component";
 @NgModule({
     imports:        [
                         BrowserModule,
                         FormsModule
                     ],
-    declarations:   [ AppComponent ],
+    declarations:   [ AppComponent, HeroDetailComponent ],
     bootstrap:      [ AppComponent ]
 })
 export class AppModule { }
